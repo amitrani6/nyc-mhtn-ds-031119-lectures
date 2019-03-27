935b8cc835503340ed01bbf56be94ba60bbcf42d
amitrani6 <amitrani1206@gmail.com>
2019-03-27 10:23:49 -0400

    remove func

--- functions.py
+++ functions.py
@@ -1,21 +1,34 @@
 # Make a function to determine if a number is odd or even
 
-def odd_even():
-    pass
+def odd_even(number):
+    if number%2==0:
+        return True
+    else:
+        return False
 
 # Make a function that takes in a list of numbers and returns the numbers that are even
 
 def even_list(numbers):
-    pass
+    even_numbers = []
+    for number in numbers:
+        if odd_even(number)==True:
+            even_numbers.append(number)
+    return even_numbers
+
 
 # Given a list return the unique names in the list
 
 def unique_names(list_of_names):
-    pass
+    unique_names=[]
+    for name in list_of_names:
+        if name.isinstance(list_of_names):
+            unique_names.append(name)
+    return unique_names
+
 
 # Make a function that determines if a word is a palindrome
 
-def palindrome_detector(string):
+#def palindrome_detector(string):
     """
     Input: string
     returns : True/False"""
@@ -25,5 +38,5 @@ print(odd_even(4))
 print(odd_even(139))
 print(even_list([1,3,4,6,7]))
 print(unique_names(['john', 'john', 'john']))
-print(palindrome_detector('racecar'))
-print(palindrome_detector('not'))
+#print(palindrome_detector('racecar'))
+#print(palindrome_detector('not'))
